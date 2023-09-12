39ead54dfed6cb94c846ea1f124924172bc28592
Jean-Baptiste Romain <jbromain25@gmail.com>
2023-09-12 17:35:42 +0200

    Ajout logs

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
